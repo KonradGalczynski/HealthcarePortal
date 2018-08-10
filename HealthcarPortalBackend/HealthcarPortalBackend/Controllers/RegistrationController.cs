e88e921dadffe0414803cb5919de017c9bb08f20
Konrad Galczynski <kgalczynski@chathamfinancial.eu>
2018-08-10 02:05:00 -0400

    initial commit with dummy service

---
+++ HealthcarPortalBackend/HealthcarPortalBackend/Controllers/RegistrationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthcarPortalBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return new OkResult();
+        }
+    }
+}
